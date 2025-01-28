99023309722e2615082f0bd7d7d9b3476643ac72
Pradeep Prajapati <154655056+Pradeep-1496@users.noreply.github.com>
2025-01-28 19:27:49 +0530

    Update App.js

--- NextStepF/App.js
+++ NextStepF/App.js
@@ -19,7 +19,7 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen
           name="Login"
           component={LoginScreen}
